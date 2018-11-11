aa3d3e5fd30e4c7f14ca1519a0283259cc259d21
michaelbz1 <mbuziewicz@efcts.us>
2018-11-10 21:23:33 -0500

    message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 
-//var cors = require('cors')
-//app.use(cors())
+var cors = require('cors')
+app.use(cors())
 
 const express = require('express');
 const path = require('path');
