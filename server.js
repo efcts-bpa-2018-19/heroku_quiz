5c05fa1c51fd68879e385444255ceece986c9680
michaelbz1 <mbuziewicz@efcts.us>
2018-11-10 21:43:13 -0500

    message

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Install express server
 
-var cors = require('cors')
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+var cors = require('cors')
 app.use(cors())
 
 //app.use(function(req, res, next) {
@@ -13,6 +14,31 @@ app.use(cors())
 //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //  next();
 //});
+/*
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.configure(function () {
+app.use(allowCrossDomain);
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(application_root, "public")));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+*/
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
@@ -20,6 +46,10 @@ app.use(express.static(__dirname + '/dist/'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
   res.sendFile(path.join(__dirname+ '/dist/index.html'));
 });
 
