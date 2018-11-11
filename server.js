dfe4647f7b30741f821b903d9a26501645932e15
michaelbz1 <mbuziewicz@efcts.us>
2018-11-10 20:40:11 -0500

    new

--- server.js
+++ server.js
@@ -16,11 +16,12 @@ app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname+ '/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
