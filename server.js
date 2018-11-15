c7432f0f1577f41f2571c925d698bc29d3b14030
michaelbz1 <mbuziewicz@efcts.us>
2018-11-15 17:47:06 -0500

    Changed comments in server.js

--- server.js
+++ server.js
@@ -6,42 +6,15 @@ const path = require('path');
 
 const app = express();
 
+//The following is probably not needed.  This was added but Asp.net project cors had to be changed and it worked.
 var cors = require('cors');
 app.use(cors());
-
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
 });
-
-
-/*
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.configure(function () {
-app.use(allowCrossDomain);
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(application_root, "public")));
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-*/
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
